a99a60fded7f2b4da60f2319209c597a70184d24
Rayssa <ralpimenta@hotmail.com>
2023-01-21 22:50:28 -0300

    realizei as configurações para aparecerem na rota

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,29 @@
 //localhost:3333
 
 import Fastify from 'fastify'
+//importar o prisma
+
+import { PrismaClient } from '@prisma/client'
 
 //criar aplicacao
 const app = Fastify()
+const prisma = new PrismaClient()
+
 /*
 *   método HTTP: Get, Post, Put, Patch, Delete
 */
+//vamos acessar por essa rota os habitos do banco
 //rotas
-app.get('/',() => {
-    return 'hello world'
+app.get('/hello',async() => {
+    
+    const habits = await prisma.habit.findMany({
+        /*where:{
+            title:{
+                startsWith: 'Beber'
+            }
+        }    */        
+    })
+    return habits
 })
 
 
